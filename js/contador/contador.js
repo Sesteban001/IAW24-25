f464c744c7cabc8fb9e67d7349ebeedf0ec5a6d9
Santiago Esteban <santiagoesteban99ebp@gmail.com>
2024-12-12 10:26:26 +0100

    creado el contador

---
+++ js/contador/contador.js
@@ -0,0 +1,11 @@
+//variable
+let cuenta = 0;
+
+//contador mostrado con F12
+console.log(cuenta);
+
+// Incremento de cuenta
+cuenta++;
+
+//cambio de contenido 
+document.getElementById("contador").innerText = cuenta;
