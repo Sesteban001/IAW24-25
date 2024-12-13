b8cef41e11eb653a2df514788b6b2ef74b1ecc13
Santiago Esteban <santiagoesteban99ebp@gmail.com>
2024-12-13 08:57:04 +0100

    total contador

--- js/contador/contador.js
+++ js/contador/contador.js
@@ -1,11 +1,33 @@
-//variable
-let cuenta = 0;
+let cuenta = 0; // Inicializa la variable cuenta
 
-//contador mostrado con F12
+function incrementar(){
+    cuenta++;
+    console.log("Mi cuenta es " + cuenta);
+    document.getElementById("contador").innerText = cuenta;
+}
+
+function guardar(){
+    document.getElementById("guardarAnteriores").innerText = cuenta;
+    document.getElementById("contador").innerText = 0;
+    cuenta = 0;
+    numerosguardados=[];
+}
+
+function totales(){
+    var contador = {};
+    // Suponiendo que tienes un array de números que deseas contar
+
+    for (var numero of numerosguardados) {
+        contador[numero] = (contador[numero] || 0) + 1;
+    }
+    console.log("Totales:", contador);
+}
+
+const BotonI = document.getElementById("botonIncrementar");
+const BotonD = document.getElementById("botonGuardar");
+
+BotonI.addEventListener('click', incrementar);
+BotonD.addEventListener('click', guardar);
+
+// Muestra el contador inicial
 console.log(cuenta);
-
-// Incremento de cuenta
-cuenta++;
-
-//cambio de contenido 
-document.getElementById("contador").innerText = cuenta;
