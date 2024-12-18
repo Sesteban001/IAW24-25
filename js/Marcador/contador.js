4dc20801ca248182faf43b1bdf4002d91d60faca
Santiago Esteban <santiagoesteban99ebp@gmail.com>
2024-12-18 10:43:14 +0100

    echo el dia 18 el marcador de baloncesto

---
+++ js/Marcador/contador.js
@@ -0,0 +1,17 @@
+// Cogemos los div de los dos marcadores
+let loc = document.getElementById("score-a");
+let vis = document.getElementById("score-b");
+
+// Función para score A = locA
+function valA(puntos) {
+    let tempA = parseInt(loc.innerText) || 0; // Convierte el texto a número, o 0 si no es un número
+    tempA += puntos; // Suma los puntos
+    loc.innerText = tempA; // Actualiza el texto del elemento
+}
+
+// Función para score B = locB
+function valB(puntos) {
+    let tempB = parseInt(vis.innerText) || 0; // Convierte el texto a número, o 0 si no es un número
+    tempB += puntos; // Suma los puntos
+    vis.innerText = tempB; // Actualiza el texto del elemento
+}
